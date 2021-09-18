71c7f8d16ec9d5227a8457f1b27d0e3ce246d595
Subham Ray <self.programmed@gmail.com>
2021-09-18 20:30:23 +0530

    initial setup

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Wrapper from "../styles/Navbar";
+import GoogleAuth from "./GoogleAuth";
+import { AppsIcon, HamburgerIcon, LogoIcon, SettingsIcon } from "./Icons";
+import Search from "./Search";
+
+function Navbar() {
+  return (
+    <Wrapper>
+      <div className="logo flex-row">
+        <HamburgerIcon className="toggle-navhandler" />
+        <span>
+          <Link to="/">
+            <LogoIcon
+              style={{
+                width: 80,
+                height: 24,
+              }}
+            />
+          </Link>
+        </span>
+      </div>
+
+      <Search />
+
+      <ul>
+        <li>
+          <AppsIcon />
+        </li>
+        <li>
+          <SettingsIcon />
+        </li>
+        <li>
+          {" "}
+          <GoogleAuth />
+        </li>
+      </ul>
+    </Wrapper>
+  );
+}
+
+export default Navbar;
